ebcf7365e712dbc652173153ab65c006fe1787b7
Davizitos57 <davi.abner57@gmail.com>
2024-07-19 13:11:49 -0300

    Diario_Atualizado.java

--- Diario_Virtual/Diario.java
+++ Diario_Virtual/Diario.java
@@ -6,14 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Diario {
-	private List<String> entradas;
+	private List<EntradaDiario> entradas;
 	private List<Usuario> usuarios;
+	private static Usuario usuarioAtual = null;
 	
     public Diario() {
         this.entradas = new ArrayList<>();
         this.usuarios = new ArrayList<>();
         Usuario admin = new Usuario("TallesProfessor", "ProfessorUFOP", true);
         usuarios.add(admin);
+    }
+    
+    public class EntradaDiario{
+    	private String texto;
+    	private Usuario autor;
+    	private String dataHora;
+    	
+    	public EntradaDiario(String entradas, Usuario autor) {
+    		this.texto = entradas;
+    		this.autor = autor;
+    		LocalDateTime agora = LocalDateTime.now();
+    	    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    	    this.dataHora = agora.format(formatador);
+    		
+    	}
+    	public String getTexto() {
+    		return texto;
+    	}
+    	public Usuario getAutor() {
+    		return autor;
+    	}
+    	public String toString() {
+    		return dataHora + " - " + autor.getUsername() + ": " + texto;
+    	}
     }
     
     public void registrarUsuario(String username, String senha, boolean isAdmin) {
@@ -28,13 +53,26 @@ public class Diario {
             }
         }
         return null;
-    }     
+    }
+    
+    public static void login(Usuario usuario) {
+    	usuarioAtual = usuario;
+    	System.out.println("Usuário " + usuario.getUsername() + " foi logado com sucesso");
+    }
+    
+    public static void logout() {
+    	System.out.println("Usuário " + usuarioAtual.getUsername() + " se deslogou do Diário\n");
+    }
 
-    public void adicionarEntrada(String entrada) {
-        LocalDateTime agora = LocalDateTime.now();
-        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        String dataFormatada = agora.format(formatador);
-        entradas.add(dataFormatada + " - " + entrada);
+    public void adicionarEntrada(String texto) {
+    	if(usuarioAtual != null) {
+    		EntradaDiario entrada = new EntradaDiario(texto, usuarioAtual);
+    		entradas.add(entrada);
+    		System.out.println("Entrada adiciona pelo perfil " + usuarioAtual.getUsername());
+    	}
+    	else {
+    		System.out.println("Nenhum usuário está logado, tente novamente mais tarde");
+    	}
     }	
 
 	public List<Usuario> getUsuarios() {
@@ -61,8 +99,8 @@ public class Diario {
 
     public void pesquisarEntradas(String palavraChave) {
         boolean encontrou = false;
-        for (String entrada : entradas) {
-            if (entrada.toLowerCase().contains(palavraChave.toLowerCase())) {
+        for (EntradaDiario entrada : entradas) {
+            if (entrada.getTexto().toLowerCase().contains(palavraChave.toLowerCase())) {
                 if (!encontrou) {
                     System.out.println("Entradas encontradas:");
                     encontrou = true;
@@ -75,17 +113,17 @@ public class Diario {
         }
     }
     public int contarPalavrasDeEntrada(String entrada) {
-        if (entrada == null || entrada.isEmpty()) {
-            return 0;
-        }
-        String[] palavras = entrada.split("\\s+");
+    	if(entrada == null || entrada.isEmpty()) {
+    		return 0;
+    	}
+    	String[] palavras = entrada.split("\\s+");
         return palavras.length;
     }
-
+    
     public int contarPalavrasTotais() {
-        int totalDePalavras = -3;
-        for (String entrada : entradas) {
-            totalDePalavras += contarPalavrasDeEntrada(entrada);
+        int totalDePalavras = 0;
+        for (EntradaDiario entrada : entradas) {
+            totalDePalavras += contarPalavrasDeEntrada(entrada.getTexto());
         }
         return totalDePalavras;
     }
