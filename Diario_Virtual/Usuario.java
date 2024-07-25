de119e34bbb2c64fc2daf384538133fa128ed2d0
Davizitos57 <davi.abner57@gmail.com>
2024-07-25 16:33:53 -0300

    Usuario_VersaoFinal.java

--- Diario_Virtual/Usuario.java
+++ Diario_Virtual/Usuario.java
@@ -4,21 +4,22 @@ public class Usuario {
     private String username;
     private String senha;
     private boolean isAdmin;
- 
+    
+    //cria um construtor para o Usuario atrelando-o um username, senha e (caso seja adm, verificando a veracidade)
     public Usuario(String username, String senha, boolean isAdmin) {
         this.username = username;
         this.senha = senha;
         this.isAdmin = isAdmin;
     }
+    //retorna o valor booleano de adm, verificando se é verdadeiro ou falso
     public boolean isAdmin(){
     	return isAdmin;
     }
-    public void setADM(boolean isAdmin){
-    	this.isAdmin = isAdmin;
-    }
+    //retorna o valor string de username, comparando se é igual a string escrita pelo usuario
     public String getUsername() {
     	return username;
     }
+    //retorna o valor string de senha, comparando se é igual a string escrita pelo usuario 
     public String getSenha() {
     	return senha;
     }
